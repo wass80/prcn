ae84715b7025c305c6bec38083360fc315452c74
wass80 <wasss80@gmail.com>
2016-04-13 00:00:11 +0000

    abc036

---
+++ atc/abc036/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int long long
+#define range(i,x,y) i=(x);i<(y);i++
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+signed main(){
+  int a,b; cin>>a>>b;
+  cout << (b-1)/a + 1 << endl;
+}
